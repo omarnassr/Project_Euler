efe272ef0d713e4211599f4e5efe9af9b9d34090
omarNasr <khlefaooo@gmail.com>
2023-01-22 18:14:40 +0200

    1st - Loop with counter

--- 1.py
+++ 1.py
@@ -3,15 +3,17 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
-
-List1 = list(range(10))
-
+#Forming an iterable data form to hold the date.
+List1 = list(range(1000))
+#Counter starting from 0 as it natural numbers only.
 sum = 0
+#For loop iterated over the list
 for i in List1:   
-            #condition required form list's items to meet.         
+            #conditions required with OR statement.         
             if i % 3 == 0 or i %5 == 0:
-                sum += i     
+                # If I matches then add it to sum every time it match. 
+                sum += i 
+#git the results                    
 print(sum)
         
-function(List1)
 
