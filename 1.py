f063389553fce1ca7b05a4619b9560aca902420e
omarNasr <khlefaooo@gmail.com>
2023-01-23 17:05:20 +0200

    modification to set the global var outisde the function

--- 1.py
+++ 1.py
@@ -6,14 +6,19 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 #Forming an iterable data form to hold the date.
 List1 = list(range(1000))
 #Counter starting from 0 as it natural numbers only.
-sum = 0
 #For loop iterated over the list
-for i in List1:   
+sum = 0   
+
+def function(list):
+    global sum
+
+    for i in list:
             #conditions required with OR statement.         
             if i % 3 == 0 or i %5 == 0:
                 # If I matches then add it to sum every time it match. 
                 sum += i 
+    print(sum)
 #git the results                    
-print(sum)
+function(List1)
         
 
